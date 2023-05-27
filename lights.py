2194170253d312f8d87434f00d49cd34a301476b
Rhythm7845~ <66195447+Rhythm7845@users.noreply.github.com>
2023-05-27 11:54:32 +0530

    Updated Renders

--- lights.py
+++ lights.py
@@ -2,5 +2,5 @@ import numpy as np
 
 # Declaes the lights in the scene.
 lights = [ 
-            {'position': np.array([1, 3, 6]),'diffuse': np.array([1.5, 1.5, 1.5]), 'specular': np.array([1, 1, 1])}
+            {'position': np.array([1, 3, 3]),'diffuse': np.array([1.5, 1.5, 1.5]), 'specular': np.array([1, 1, 1])}
 ]
