7ad676949fc10cbc7c83f60214a89263493e7306
Rhythm7845~ <66195447+Rhythm7845@users.noreply.github.com>
2023-05-27 13:17:04 +0530

    Update

--- raytracing.py
+++ raytracing.py
@@ -18,7 +18,7 @@ gamma = 2.2
 
 camera = np.array([0, 0, 1])
 ratio = float(width/height)
-screen = (-1, 1/ratio, 1, -1/ratio)  # Sccreen perfectly fits the camera.
+screen = (-1, 1/ratio, 1, -1/ratio)  # Screen perfectly fits the camera.
 
 # Main RayTracing loop.
 image = np.zeros((height, width, 3))
@@ -30,8 +30,7 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
         color = np.zeros((3))
         reflection = 1
         for k in range(max_depth):
-            nearest_object, min_distance = f.nearest_intersected_object(
-                objects, origin, direction)
+            nearest_object, min_distance = f.nearest_intersected_object(objects, origin, direction)
             if nearest_object is None:
                 break
             intersection = origin + min_distance * direction
@@ -42,19 +41,15 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
             for light in lights:
                 intersection_to_light = f.normalise(
                     light['position'] - shifted_point)
-                _, min_distance = f.nearest_intersected_object(
-                    objects, shifted_point, intersection_to_light)
-                intersection_to_light_distance = np.linalg.norm(
-                    light['position'] - intersection)
+                _, min_distance = f.nearest_intersected_object(objects, shifted_point, intersection_to_light)
+                intersection_to_light_distance = np.linalg.norm(light['position'] - intersection)
                 is_shadowed = min_distance < intersection_to_light_distance
                 if is_shadowed:
                     continue
-                illumination += nearest_object['diffuse'] * light['diffuse'] * (
-                    1 - 0.63661977236759*np.arccos(np.dot(intersection_to_light, normal_to_surface)))
+                illumination += nearest_object['diffuse'] * light['diffuse'] * (1 - 0.63661977236759*np.arccos(np.dot(intersection_to_light, normal_to_surface)))
                 intersection_to_camera = f.normalise(camera - intersection)
                 H = f.normalise(intersection_to_light + intersection_to_camera)
-                illumination += nearest_object['specular'] * light['specular'] * np.dot(
-                    normal_to_surface, H) ** (nearest_object['shininess'] / 4)
+                illumination += nearest_object['specular'] * light['specular'] * np.dot(normal_to_surface, H) ** (nearest_object['shininess'] / 4)
             color += reflection * illumination
             reflection *= nearest_object['reflection'] * \
                 nearest_object['diffuse']*illumination
