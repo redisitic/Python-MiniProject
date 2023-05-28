6684952159fae0afd05eed73f7afef0f6534b581
Rhythm7845~ <66195447+Rhythm7845@users.noreply.github.com>
2023-05-28 12:26:24 +0530

    Update lights.py

--- lights.py
+++ lights.py
@@ -5,8 +5,7 @@ class Light:
         self.diffuse = np.array(diffuse)
         self.specular = np.array(specular)
         
-light1 = Light([1, 3, 3], [1.5, 1.5, 1.5], [1, 1, 1])
-
 def get_lights():
+    light1 = Light([1, 3, 3], [1.5, 1.5, 1.5], [1, 1, 1])
     return [light1]
     
