ea46bcf3948ac4fe40f3fe5afc30934abd74c32d
Spaceginner <ivan.demian2009@gmail.com>
2023-06-17 14:31:30 +0500

    make it to just return a list of objects immediately without all of those shenanigans

--- lights.py
+++ lights.py
@@ -1,15 +1,19 @@
 import numpy as np
 from spheres import get_objects
 
+
 class Light:
     def __init__(self, position, diffuse, specular, intensity):
         self.position = np.array(position)
-        self.diffuse = np.array(diffuse)*intensity
+        self.diffuse = np.array(diffuse) * intensity
         self.specular = np.array(specular)
-        
-def get_lights():
-    light1 = Light([1, 2, 0], [1, 1, 1], [1, 1, 1], 10)
-    return [light1]
+
+
+def get_lights() -> list[Light]:
+    return [
+        Light([1, 2, 0], [1, 1, 1], [1, 1, 1], 10)
+    ]
+
 
 def check_lights():
     for light in get_lights():
