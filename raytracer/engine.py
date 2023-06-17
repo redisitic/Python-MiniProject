dafb14d1d50fe61303448ab53f9ebd2222cd16e1
Spaceginner <ivan.demian2009@gmail.com>
2023-06-17 20:59:24 +0500

    multiprocessing + progress bar

--- raytracer/engine.py
+++ raytracer/engine.py
@@ -1,4 +1,7 @@
+from multiprocessing import Pool
+
 import numpy as np
+from tqdm import tqdm
 
 from .scene import Scene, Sphere
 
@@ -34,53 +37,71 @@ def _reflected(vector, axis):
     return vector - 2 * np.dot(vector, axis) * axis
 
 
+def _render_column(packed_args):
+    res, camera_pos, max_depth, spheres, lights, exposure, gamma, hdr, screen, y, i, column = packed_args
+
+    for j, x in enumerate(np.linspace(screen[0], screen[2], res[0])):
+        pixel = np.array([x, y, 0])
+        origin = camera_pos
+        direction = _normalise(pixel - origin)
+        color = np.zeros(3)
+        reflection = 1
+
+        for k in range(max_depth):
+            nearest_object: Sphere
+            nearest_object, min_distance = _nearest_intersected_object(spheres, origin, direction)
+            if nearest_object is None:
+                break
+            intersection = origin + min_distance * direction
+            normal_to_surface = _normalise(intersection - nearest_object.center)
+            shifted_point = intersection + 1e-5 * normal_to_surface
+            illumination = np.zeros(3)
+            shadowed_illumination = np.zeros(3)
+            for light in lights:
+                intersection_to_light = _normalise(light.position - shifted_point)
+                _, min_distance = _nearest_intersected_object(spheres, shifted_point, intersection_to_light)
+                intersection_to_light_distance = np.linalg.norm(light.position - intersection)
+                is_shadowed = min_distance < intersection_to_light_distance
+                if is_shadowed:
+                    reflection *= 0.25 * (nearest_object.diffuse * nearest_object.reflection) * (light.diffuse * (
+                            1 - 0.63661977236759 * np.arccos(np.dot(intersection_to_light,
+                                                                    normal_to_surface))) / intersection_to_light_distance ** 2)
+                    shadowed_illumination += reflection
+                illumination += nearest_object.diffuse * light.diffuse * (1 - 0.63661977236759 * np.arccos(
+                    np.dot(intersection_to_light, normal_to_surface))) / intersection_to_light_distance ** 2
+                intersection_to_camera = _normalise(camera_pos - intersection)
+                illumination += nearest_object.specular * light.specular * np.dot(normal_to_surface, _normalise(
+                    intersection_to_light + intersection_to_camera)) ** (nearest_object.shininess / 4)
+            color += (reflection * illumination) + shadowed_illumination
+            reflection *= nearest_object.reflection * nearest_object.diffuse * illumination
+            origin = shifted_point
+            direction = _reflected(direction, normal_to_surface)
+
+        color = color * exposure - gamma + 2.2
+        if hdr:
+            color = color ** 0.6
+        column[j] = np.clip(color, 0, 1)
+
+    return column
+
+
 def render(scene: Scene, quite: bool = False):
     ratio = scene.res[0] / scene.res[1]
-    screen = (-1, 1/ratio, 1, -1/ratio)
+    screen = (-1, 1 / ratio, 1, -1 / ratio)
 
     image = np.zeros((scene.res[1], scene.res[0], 3))
-    for i, y in enumerate(np.linspace(screen[1], screen[3], scene.res[1])):
-        for j, x in enumerate(np.linspace(screen[0], screen[2], scene.res[0])):
-            pixel = np.array([x, y, 0])
-            origin = scene.get_camera().position
-            direction = _normalise(pixel - origin)
-            color = np.zeros(3)
-            reflection = 1
-            for k in range(scene.max_depth):
-                nearest_object: Sphere
-                nearest_object, min_distance = _nearest_intersected_object(scene.get_spheres(), origin, direction)
-                if nearest_object is None:
-                    break
-                intersection = origin + min_distance * direction
-                normal_to_surface = _normalise(intersection - nearest_object.center)
-                shifted_point = intersection + 1e-5 * normal_to_surface
-                illumination = np.zeros(3)
-                shadowed_illumination = np.zeros(3)
-                for light in scene.get_lights():
-                    intersection_to_light = _normalise(light.position - shifted_point)
-                    _, min_distance = _nearest_intersected_object(scene.get_spheres(), shifted_point, intersection_to_light)
-                    intersection_to_light_distance = np.linalg.norm(light.position - intersection)
-                    is_shadowed = min_distance < intersection_to_light_distance
-                    if is_shadowed:
-                        reflection *= 0.25 * (nearest_object.diffuse * nearest_object.reflection) * (light.diffuse * (1 - 0.63661977236759*np.arccos(np.dot(intersection_to_light, normal_to_surface))) / intersection_to_light_distance ** 2)
-                        shadowed_illumination += reflection
-                    illumination += nearest_object.diffuse * light.diffuse * (1 - 0.63661977236759*np.arccos(np.dot(intersection_to_light, normal_to_surface))) / intersection_to_light_distance ** 2
-                    intersection_to_camera = _normalise(scene.get_camera().position - intersection)
-                    illumination += nearest_object.specular * light.specular * np.dot(normal_to_surface, _normalise(intersection_to_light + intersection_to_camera)) ** (nearest_object.shininess / 4)
-                color += (reflection * illumination) + shadowed_illumination
-                reflection *= nearest_object.reflection * nearest_object.diffuse * illumination
-                origin = shifted_point
-                direction = _reflected(direction, normal_to_surface)
 
-            color = color * scene.exposure - scene.gamma + 2.2
-            if scene.hdr:
-                color = color**0.6
-            image[i, j] = np.clip(color, 0, 1)
+    arguments = [
+        (scene.res, scene.get_camera().position, scene.max_depth, scene.get_spheres(), scene.get_lights(), scene.exposure, scene.gamma, scene.hdr, screen, y, i, image[i])
+        for i, y in enumerate(np.linspace(screen[1], screen[3], scene.res[1]))
+    ]
 
-        if not quite:
-            print(f"{i+1} / {scene.res[1]}, {(i+1)/scene.res[1]:.2%}", end="\r")
-
-    if not quite:
-        print("\n")
+    with tqdm(total=scene.res[1] * scene.res[0], unit='px', disable=quite,
+              bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}, {rate_fmt}{postfix}]"  # custom bar format to not include the ETA
+              ) as pbar:
+        with Pool() as p:
+            for i, column in enumerate(p.imap(_render_column, arguments)):
+                image[i] = column
+                pbar.update(scene.res[0])
 
     return image
